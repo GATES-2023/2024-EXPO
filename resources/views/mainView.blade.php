1b965f46d592e5f0491218b19bd0dc5a631a648c
Will  <speedbolt1004@gmail.com>
2024-06-09 14:13:26 +0700

    apshipashi

--- resources/views/mainView.blade.php
+++ resources/views/mainView.blade.php
@@ -97,36 +97,63 @@
 
             
         </div>
-        <img class="absolute bottom-10 sm:bottom-5 left-20 md:left-20 sm:left-10 lg:left-40" src="heromenu.png" alt="Hero Menu" width="200" height="200">
-        <img style="transform: scale(-1, 1)" class="absolute bottom-10 sm:bottom-5 right-20 md:right-10 lg:right-40 sm:right-20" src="heromenu.png" alt="Hero Menu" width="200" height="200">
+        <img class="absolute bottom-10 sm:bottom-5 left-5 md:left-20 sm:left-10 lg:left-40 sm:w-[210px] md:w-[270px] w-[180px]" src="heromenu.png" alt="Hero Menu">
+        <img style="transform: scale(-1, 1)" class="absolute bottom-10 sm:bottom-5 right-5 md:right-10 lg:right-40 sm:right-10  sm:w-[210px] md:w-[270px] w-[180px]" src="heromenu.png" alt="Hero Menu">
     </div>
 
     <!-- about resto -->
-    <div class=" bg-white h-full md:py-20 opacity-0 transition-opacity ease-in-out duration-1000 fade-section">
+
+    <!-- <div class=" bg-white h-full md:py-20 opacity-0 transition-opacity ease-in-out duration-1000 fade-section">
 
         <div class="card flex flex-col md:flex-row sm:flex-columns-reverse px-10 py-10">
             <img class=" flex justify-center rounded-lg max-h-100" src="kitchen.png" alt="About Us" height="300" width="700">
             
             <div class="card-detail px-10 sm:px-0 md:px-10">
                 <div class="top font-eb-garamond text-7xl pt-10 md:py-0">Supa fresh from Sulawesi.</div>
-                <div class="middle py-10 md:py-10 text-l">Where Sulawesi meets culinary expertise, fresh ingredients straight from the source. Every bite tells a story of Indonesia's nature.
+                <div class="middle py-10 md:py-10 text-l">
+                    Where Sulawesi meets culinary expertise, fresh ingredients straight from the source. Every bite tells a story of Indonesia's nature.
                     Feel the warmth of Sulawesi's culture in every bite. Only here!    
                     <br><br>
-                    <br>Curious to know more about our secret sauce? <br>Explore further on this page.</div>
+                    <br>Curious to know more about our secret sauce? <br>Explore further on this page.
+                </div>
             </div>
         </div>
+    </div> -->
+
+    <div class="bg-white h-full md:py-20 opacity-0 transition-opacity ease-in-out duration-1000 fade-section">
+        <div class="card flex flex-col md:flex-row sm:flex-columns-reverse sm:px-10 py-10 md:space-x-10">
+            <!-- image -->
+            <div class="flex justify-start md:w-1/2 px-10">
+                <img class="rounded-lg max-h-100 w-full object-cover object-center" src="kitchen.png" alt="About Us" />
+            </div>
+
+            <!-- details -->
+            <div class="card-detail text-zinc-900 md:w-1/2 px-10 sm:px-0">
+                <div class="top font-eb-garamond text-4xl sm:text-5xl md:text-7xl py-10 md:py-0">Supa fresh from Sulawesi.</div>
+                <div class="middle pt-10 md:py-10 text-base sm:text-lg md:text-xl">
+                    Where Sulawesi meets culinary expertise, fresh ingredients straight from the source. Every bite tells a story of Indonesia's nature. Feel the warmth of Sulawesi's culture in every bite. Only here!    
+                    <br><br>
+                    Curious to know more about our secret sauce?<br>Explore further on this page.
+                </div>
+                
+            </div>
+
+        </div>
     </div>
+    
 
     <!-- menu introduction -->
-    <div class=" bg-emerald-950 h-full md:py-20 opacity-0 transition-opacity ease-in-out duration-1000 fade-section">
-
-        <div class="card flex flex-col md:flex-row px-10 py-10">
-            <div class="card-detail text-white px-10 md:px-10 sm:px-0">
-                <div class="top font-eb-garamond text-7xl py-10 md:py-0">Appetite<br>Suppressant.</div>
-                <div class="middle pt-10 md:py-10 text-l">
+    <div class="bg-emerald-950 h-full md:py-20 opacity-0 transition-opacity ease-in-out duration-1000 fade-section">
+        <div class="card flex flex-col md:flex-row sm:px-10 py-10 md:space-x-10">
+            
+            <!-- details -->
+            <div class="card-detail text-white md:w-1/2 px-10 sm:px-0">
+                <div class="top font-eb-garamond text-4xl sm:text-5xl md:text-7xl py-10 md:py-0">Appetite<br>Suppressant.</div>
+                <div class="middle pt-10 md:py-10 text-base sm:text-lg md:text-xl">
                     Enjoy delicious food at our restaurant where everyone can delight in tasty dishes. Each bite is designed to satisfy your stomach. Importantly, we ensure all our food is 100% halal, respecting your beliefs and dietary needs.
                     <br><br>
-                    Rest assured, your trust and satisfaction are our top priorities.</div>
+                    Rest assured, your trust and satisfaction are our top priorities.
+                </div>
                 <div class="flex w-full py-10">
                     <div class="bg-yellow-100 hover:bg-yellow-200 rounded-full transition ease-in-out delay-100 hover:scale-110 duration-200">
                         <a href="#" target="_blank" class="block py-3 px-8 text-center text-l font-medium text-zinc-900">
@@ -136,10 +163,13 @@
                 </div>
             </div>
 
-            <img class=" flex justify-center rounded-lg max-h-100" src="sate.png" alt="About Us" height="300" width="700">
-
+            <!-- image -->
+            <div class="flex justify-start md:w-1/2 px-10">
+                <img class="rounded-lg max-h-100 w-full object-cover object-left" src="sate.png" alt="About Us" />
+            </div>
         </div>
     </div>
+
 
     <!-- best seller section -->
     <div id="bests" class=" flex flex-col items-center justify-center bg-zinc-900 h-[550px] overflow-hidden space-y-10">
@@ -147,21 +177,21 @@
         <div class="group saturate-0 hover:saturate-100 swiper swiper-container">
             <div class="swiper-wrapper py-5">
                 <div class="swiper-slide">
-                    <div class="flex space-x-16 md:space-x-16 sm:space-x-0 items-center justify-center">
-                        <img class="w-[220px] h-[220px]" src="fav1.png" />
-                        <img class="w-[220px] h-[220px]" src="fav2.png" />
-                        <img class="w-[220px] h-[220px]" src="fav3.png" />
-                        <img class="w-[220px] h-[220px]" src="fav4.png" />
-                        <img class="w-[220px] h-[220px]" src="fav5.png" />
+                    <div class="flex space-x-0 md:space-x-16 sm:space-x-8 items-center justify-center">
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav1.png" />
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav2.png" />
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav3.png" />
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav4.png" />
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav5.png" />
                     </div>
                 </div>
                 <div class="swiper-slide ">
-                    <div class="flex space-x-16 md:space-x-16 sm:space-x-0 items-center justify-center">
-                        <img class="w-[220px] h-[220px]" src="fav1.png" />
-                        <img class="w-[220px] h-[220px]" src="fav2.png" />
-                        <img class="w-[220px] h-[220px]" src="fav3.png" />
-                        <img class="w-[220px] h-[220px]" src="fav4.png" />
-                        <img class="w-[220px] h-[220px]" src="fav5.png" />
+                    <div class="flex space-x-0 md:space-x-16 sm:space-x-8 items-center justify-center">
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav1.png" />
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav2.png" />
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav3.png" />
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav4.png" />
+                        <img class="h-28 sm:w-30 sm:h-30 md:w-40 md:h-40" src="fav5.png" />
                     </div>
                 </div>
             </div>
@@ -176,8 +206,7 @@
     </div>
 
     <!-- maps section -->
-    <div id="maps" class=" flex bg-yellow-100 h-screen">
-        
+    <!-- <div id="maps" class=" flex bg-yellow-100 h-screen">
         <div class=" flex items-center justify-center">
             <div class="card-detail px-10 py-10">
                 <div class="top font-eb-garamond text-7xl py-10 md:py-0">
@@ -204,19 +233,48 @@
         </div>
 
         <div class="relative w-full">
-            <!-- batam -->
-        <!-- <iframe class="absolute top-0 left-0 w-full h-full"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15955.266709937522!2d104.040160!3d1.113169!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d98b0626892bb1%3A0x3d2f00314632707e!2sBatam%2C%20Kepulauan%20Riau%2C%20Indonesia!5e0!3m2!1sen!2sus!4v1648482801994!5m2!1sen!2sus"
-            frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
-        </iframe> -->
+            <iframe class="absolute top-0 left-0 w-full h-full"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7933.484991105166!2d106.827183!3d-6.175392!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f6e7e71f6e43%3A0x2e2f5e7a69b5b745!2sJakarta%2C%20Indonesia!5e0!3m2!1sen!2sus!4v1648483010939!5m2!1sen!2sus"
+                frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+            </iframe>
+        </div>
+    </div> -->
 
-        <!-- indonesia -->
-        <iframe class="absolute top-0 left-0 w-full h-full"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7933.484991105166!2d106.827183!3d-6.175392!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f6e7e71f6e43%3A0x2e2f5e7a69b5b745!2sJakarta%2C%20Indonesia!5e0!3m2!1sen!2sus!4v1648483010939!5m2!1sen!2sus"
-            frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
-        </iframe>
+    <div id="maps" class="bg-yellow-100 h-full md:py-20 opacity-0 transition-opacity ease-in-out duration-1000 fade-section">
+        <div class="card flex flex-col sm:flex-columns-reverse md:flex-row sm:px-10 py-10 md:space-x-10">
+            
+            <!-- details -->
+            <div class="card-detail text-zinc-900 md:w-1/2 px-10 sm:px-0">
+                <div class="top font-eb-garamond text-4xl sm:text-5xl md:text-7xl py-10 md:py-0">The adventure awaits!!</div>
+                <div class="middle pt-10 md:py-10 text-base sm:text-lg md:text-xl">
+                    Don't wait any longer! 
+                    Experience the deliciousness of our restaurant's food. From the tasty dishes to a cozy atmosphere, we're ready to pamper your taste buds. 
+                    <br> <br>
+                    Find joy in every bite and enjoy an unforgettable dining experience with us.
+                    <br>
+                    Come on, visit our restaurant today and make your evening special!
+                </div>
+                <div class="flex w-full py-10">
+                    <div class="bg-zinc-900 hover:bg-zinc-700 rounded-full transition ease-in-out delay-100 hover:scale-110 duration-200">
+                        <a href="https://maps.app.goo.gl/CdnZNXiA3os8Mn3v8" target="_blank" class="block py-3 px-8 text-center text-l font-medium text-white">
+                            Take a look at maps
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- image -->
+            <div class="flex justify-start md:w-1/2 px-10">
+                <div class="relative w-full">
+                    <iframe class="w-full h-full rounded-lg object-cover"
+                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7933.484991105166!2d106.827183!3d-6.175392!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f6e7e71f6e43%3A0x2e2f5e7a69b5b745!2sJakarta%2C%20Indonesia!5e0!3m2!1sen!2sus!4v1648483010939!5m2!1sen!2sus"
+                        frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0">
+                    </iframe>
+                </div>
+            </div>
         </div>
     </div>
+
 
     <!-- footer -->
     <div class=" relative flex-col bg-emerald-950 h-[400px] w-full md:h-[300px] sm:h-[300px] py-10 px-10 space-y-4">
@@ -255,6 +313,7 @@
 
     <!-- navbar config -->
     <script>
+        // swiper
         document.getElementById('menu-toggle').addEventListener('click', function() {
             var menu = document.getElementById('navbar-sticky');
             menu.classList.toggle('hidden');
@@ -274,7 +333,8 @@
             spaceBetween: 10,
             });
         });
-
+        
+        // scroll to anchor
         document.addEventListener('DOMContentLoaded', () => {
             const links = document.querySelectorAll('a[data-scroll]');
 
@@ -294,6 +354,7 @@
             });
         });
 
+        // fade
         document.addEventListener('DOMContentLoaded', () => {
             const sections = document.querySelectorAll('.fade-section');
             
